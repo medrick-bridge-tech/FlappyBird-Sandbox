75fc881f2a1cbe56d39e399700afb5360db0d8b4
Siavash Namjoo <namjoosiavash@outlook.com>
2023-08-19 16:03:15 +0330

    player jump and move forward

---
+++ Assets/_Script/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _rbPlayer;
+    private bool isJumping = true;
+    [SerializeField] float jumpForcce;
+    [SerializeField] float forwardForce;
+
+    void Start()
+    {
+        _rbPlayer = GetComponent<Rigidbody2D>();
+    }
+
+     void Update()
+    {
+        Jump();
+        MoveForward();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rbPlayer.AddForce(Vector2.up * jumpForcce, ForceMode2D.Impulse);
+            isJumping = false;
+        }
+    }
+
+    void MoveForward()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rbPlayer.AddForce(Vector2.right * forwardForce,ForceMode2D.Impulse);
+        }
+    }
+}
